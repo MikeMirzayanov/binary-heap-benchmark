cfd4d34ca1d28100b2573f0e779a874a5532d0f0
unknown <mike@t420s-win7ent.(none)>
2013-12-19 03:30:39 +0400

    - improved pascal code

--- pascal/heap.pas
+++ pascal/heap.pas
@@ -1,17 +1,23 @@
-const MAXN = 10000000;
+uses
+    SysUtils, DateUtils;
 
-var h: array[0..MAXN - 1] of longInt;
+const
+    MAXN = 10000000;
 
-procedure pushDown(pos, n: longInt);
-var j, t: longInt;
+var
+    h: array[0..MAXN - 1] of LongInt;
+
+procedure pushDown(pos, n: LongInt);
+var
+    j, t: LongInt;
 begin
-    while (2 * pos + 1 < n) do
+    while 2 * pos + 1 < n do
     begin
         j := 2 * pos + 1;
         if (j + 1 < n) and (h[j + 1] > h[j]) then
             inc(j);
 
-        if (h[pos] >= h[j]) then
+        if h[pos] >= h[j] then
             break;
 
         t := h[pos];
@@ -22,17 +28,20 @@ begin
     end;
 end;
 
-var i, n, t: longInt;
-
+var
+    i, n, t: LongInt;
+    start: TDateTime;
 begin
+    start := now;
+
     for i := 0 to MAXN - 1 do
         h[i] := i;
 
-    for i := (MAXN div 2) downTo 0 do
+    for i := (MAXN div 2) downto 0 do
         pushDown(i, MAXN);
 
     n := MAXN;
-    while (n > 1) do
+    while n > 1 do
     begin
         t := h[0];
         h[0] := h[n - 1];
@@ -43,6 +52,10 @@ begin
     end;
 
     for i := 0 to MAXN - 1 do
-        if (h[i] <> i) then
-            writeln( 'fail: h[', i, '] != ', i );
+        if h[i] <> i then begin
+            writeln('fail: h[', i, '] != ', i);
+            halt(1);
+        end;
+
+    writeln('Done in ', MilliSecondsBetween(start, now));
 end.
