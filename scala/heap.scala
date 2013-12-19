ff7e3b0c00652f927508978cfaa2b0b1c2b1cb98
unknown <mike@t420s-win7ent.(none)>
2013-12-19 13:58:27 +0400

    - improved Scala implementation to be closer to C++/Java/JavaScript

--- scala/heap.scala
+++ scala/heap.scala
@@ -1,7 +1,6 @@
 // Author: Wizmann
-// Done in 1139(ms) on Intel(R) Core(TM) i5-3230M CPU @ 2.60GHz
 
-import scala.annotation.tailrec
+import scala.util.control.Breaks._
 
 object Heap {
     val N = 10000000
@@ -12,32 +11,31 @@ object Heap {
 
         (0 to N/2).reverse.foreach(pushDown(_, N))
         (2 to N).reverse.foreach(i => {
-            arraySwap(h, 0, i - 1);
+            val t = h(0);
+            h(0) = h(i - 1);
+            h(i - 1) = t;
             pushDown(0, i - 1)
         })
         (0 until N).foreach(x => assert(h(x) == x))
 
         val end = System.nanoTime
-        println("Done in " + ((end - st)/1e6).toLong + "(ms)")
+        println("Done in " + ((end - st)/1e6).toLong)
     }
 
-    @tailrec
     def pushDown(pos: Int, n: Int) {
-        val j = left(pos);
-        if (j < n) {
-            val next = if (j + 1 < n && h(j + 1) > h(j)) j + 1 else j
-
-            if (h(pos) < h(next)) {
-                arraySwap(h, pos, next)
-                pushDown(next, n)
+        var p = pos;
+        breakable { 
+            while (2 * p + 1 < n) {
+                var j = 2 * p + 1;
+                if (j + 1 < n && h(j + 1) > h(j))
+                    j += 1;
+                if (h(p) >= h(j))
+                    break;
+                val t = h(p);
+                h(p) = h(j);
+                h(j) = t;
+                p = j;
             }
         }
     }
-    def arraySwap(ll: Array[Int], a: Int, b: Int) {
-        val t = ll(a)
-        ll(a) = ll(b)
-        ll(b) = t
-    }
-    def left(x: Int): Int = 2 * x + 1
-    def right(x: Int): Int = 2 * x + 2
 }
