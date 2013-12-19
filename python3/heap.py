6dd48f89cdf141df86ff330317d138753cb6e731
unknown <mike@t420s-win7ent.(none)>
2013-12-19 12:16:03 +0400

    - improved Python implementation, they are a little bit faster now

--- python3/heap.py
+++ python3/heap.py
@@ -1,41 +1,26 @@
-#!/usr/bin/env python
-
 import time
-
-N = 10000000
-
-h = [None] * N
-
-def pushDown(pos, n):
-    while 2*pos + 1 < n:
-        j = 2*pos + 1
-        if (j+1 < n) and (h[j+1] > h[j]):
+ 
+def pushDown(h, pos, n):
+    while 2 * pos + 1 < n:
+        j = 2 * pos + 1
+        if j + 1 < n and h[j+1] > h[j]:
             j += 1
         if h[pos] >= h[j]:
             break
         h[pos], h[j] = h[j], h[pos]
         pos = j
-
-def main():
+ 
+if __name__ == "__main__":
     start = time.clock()
-
-    for i in range(N):
-        h[i] = i
-
+    N = 10000000
+    h = list(range(N))
     for i in range(N//2, -1, -1):
-        pushDown(i, N)
-
+        pushDown(h, i, N)
     n = N
     while n > 1:
-        h[0], h[n-1] = h[n-1], h[0]
         n -= 1
-        pushDown(0, n)
-
+        h[0], h[n] = h[n], h[0]
+        pushDown(h, 0, n)
     for i in range(N):
-        if h[i] != i:
-            raise Exception("h[i] != i")
-
-    print("Done in", (time.clock() - start) * 1000)
-
-if __name__ == '__main__':
-    main()
+        assert(h[i] == i)
+    print("Done in %f" % ((time.clock() - start) * 1000))
